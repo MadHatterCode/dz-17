fcd6356ab95764adf505b00119e3048cb32d4f9a
Sergii <sadante@gmail.com>
2024-03-09 11:50:01 +0200

    dz-17 solution

---
+++ dz-17.py
@@ -0,0 +1,12 @@
+test_sting = "there is vomit on his sweater already"
+test_sting_2 = "greetings, friends"
+
+
+def correct_sentence(text):
+    text_end = ""
+    if text[-1] not in "!?.,;:":
+        text_end = "."
+    return print(text[0].upper() + text[1::] + text_end)
+
+
+correct_sentence(test_sting_2)
